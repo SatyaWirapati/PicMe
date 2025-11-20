fc5cc99a68ded975220d09416921fb4c96f1cb04
SatyaWirapati <satyathewise@gmail.com>
2025-11-20 11:24:47 +0800

    refactor: change ExplorePage & HomePage into using PostGrid

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,6 +1,7 @@
 import useExplorePosts from "../hooks/useExplorePosts";
 import { useEffect } from "react";
 import useInfiniteScroll from "../hooks/useInfiniteScroll";
+import PostGrid from "../components/post/PostGrid";
 
 const ExplorePage = () => {
     const { posts, getPosts, loading, hasMore } = useExplorePosts(9);
@@ -8,23 +9,7 @@ const ExplorePage = () => {
     return (
         <div >
             <h2 className="font-semibold text-lg mg-3">Explore</h2>
-            <div className="grid grid-cols-3 gap-4">
-                {posts.map((p) => (
-                    <div key={p.id} className="aspect-square bg-gray-200 rounded-md">
-                        {p.imageUrl && (
-                            <img
-                                src={p.imageUrl}
-                                onError={(e) => {
-                                    e.target.src =
-                                        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=500";
-                                }}
-                                className="w-full h-full object-cover rounded-md"
-                            />
-
-                        )}
-                    </div>
-                ))}
-            </div>
+            <PostGrid posts={posts}/>
             <div ref={loaderRef} className="h-10"></div>
             {loading && <p className="text-center py-3 text-sm">Loading... </p>}
         </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,13 @@
 import useHomePosts from "../hooks/useHomePosts";
 import useInfiniteScroll from "../hooks/useInfiniteScroll";
+import PostGrid from "../components/post/PostGrid";
 const HomePage = () => {
     const {posts, getPosts, loading, hasMore} = useHomePosts(9);
     const loaderRef = useInfiniteScroll(getPosts, loading, hasMore);
     return (
         <div >
             <h2 className="font-semibold text-lg mg-3">Explore</h2>
-            <div className="grid grid-cols-3 gap-4">
-                {posts.map((p) => (
-                    <div key={p.id} className="aspect-square bg-gray-200 rounded-md">
-                        {p.imageUrl && (
-                            <img
-                                src={p.imageUrl}
-                                onError={(e) => {
-                                    e.target.src =
-                                        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=500";
-                                }}
-                                className="w-full h-full object-cover rounded-md"
-                            />
-
-                        )}
-                    </div>
-                ))}
-            </div>
+            <PostGrid posts={posts}/>
             <div ref={loaderRef} className="h-10"></div>
             {loading && <p className="text-center py-3 text-sm">Loading... </p>}
         </div>
