69fc4713abe2b992cb873788b48c2b96dcfbd60d
Satya Wirapati <satyathewise@gmail.com>
2025-11-29 18:15:45 +0800

    feat: add ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const ProtectedRoute = () => {
+    const { isAuthenticated } = useAuth();
+    if (isAuthenticated) {
+        return <Outlet />;
+    }
+
+    return <Navigate to="/login" />
+}
+
+export default ProtectedRoute;
