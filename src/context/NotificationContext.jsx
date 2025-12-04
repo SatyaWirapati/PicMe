a046911afe36e7a38c2ec27346a0ec80776244ec
Satya Wirapati <satyathewise@gmail.com>
2025-12-04 14:25:36 +0800

    feat:add NotificationContext.jsx

---
+++ src/context/NotificationContext.jsx
@@ -0,0 +1,22 @@
+import { useContext, createContext, useState } from "react";
+import Notification from "../components/Notification";
+
+const NotificationContext = createContext();
+
+export const useNotification = () => useContext(NotificationContext);
+
+export const NotificationProvider = ({ children }) => {
+  const [message, setMessage] = useState("");
+
+  const showNotification = (msg, duration = 3000) => {
+    setMessage(msg);
+    setTimeout(() => setMessage(""), duration);
+  };
+
+  return (
+    <NotificationContext.Provider value={showNotification}>
+      {children}
+      <Notification message={message} />
+    </NotificationContext.Provider>
+  );
+};
