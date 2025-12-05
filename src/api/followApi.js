b4ef59e135598d4d925bf94d336bcac514975d1e
Satya Wirapati <satyathewise@gmail.com>
2025-12-05 22:02:35 +0800

    feat: add followApi.js

---
+++ src/api/followApi.js
@@ -0,0 +1,14 @@
+import axiosInstance from "../utils/axiosInstance";
+
+const getFollowingById = async (userId, page=1,size=10 ) => {
+    const response = await axiosInstance.get(`/following/${userId}`, {
+        params: { page, size },
+    });
+    return response.data
+}
+
+const getFollowersById = async (userId, page = 1, size = 10) => {
+    const response = await axiosInstance.get(`/followers/${userId}`, {
+        params: { page, size },
+    })
+}
